8254b5c8811e1998d2e9774938bdf45a533432c3
Vinny Martinez <vinny@vinnymartinez.com>
2017-01-27 18:38:37 -0500

    gifs

--- javascripts/connect4.js
+++ javascripts/connect4.js
@@ -1,3 +1,20 @@
+var backgrounds = [
+  "http://i.giphy.com/26BRLblDUw8VAhoFq.gif",
+  "http://i.giphy.com/qBhCZARUiS5WM.gif",
+  "http://i.giphy.com/11fSxqu3FUp2M0.gif",
+  "http://i.giphy.com/tzrlO0JiwjS7e.gif",
+  "http://i.giphy.com/3oriOeBHxdF9FokwJq.gif",
+  "http://i.giphy.com/mcT6IxQicH7hu.gif",
+  "http://i.giphy.com/l3q2ZlDOphy540eli.gif",
+  "http://i.giphy.com/26xBzu2ogAunL19hS.gif",
+  "http://i.giphy.com/NxWk4NY7C59Fm.gif",
+  "http://i.giphy.com/dGBiI0Wx9WPte.gif",
+  "http://i.giphy.com/BlcWQ9L2VfOFO.gif",
+  "http://i.giphy.com/3oEduH8ySCZMrUdytW.gif",
+  "http://i.giphy.com/UsqR2a5lOCzAI.gif",
+  "http://i.giphy.com/CLkMWh66GNkm4.gif"
+]
+
 class Connect4 {
   constructor() {
     this.board = new Board()
@@ -28,7 +45,7 @@ class Connect4 {
       if (children[i].getAttribute('data-fill') === 'false')
       currentChild = children[i]
     }
-
+    $('body').css('background-image', `url(${backgrounds[Math.floor(Math.random() * backgrounds.length)]})`)
     if (this.playCount % 2 === 0) {
       $(currentChild).css('background-color', 'red')
       $(currentChild).attr('data-fill', 'true')
