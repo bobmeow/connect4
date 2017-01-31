549ba071172d0d39476daa006a2509a9ab5a10e8
limerentfox <christi@limerentfox.com>
2017-01-30 21:27:41 -0500

    styles

--- index.html
+++ index.html
@@ -3,26 +3,30 @@
   <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
+<section class="blur">
+
+
 <div class="text">
-  <h1>Connect for</h1>
+  <h1>Built with JS<br><span data-shadow-text="Text-Shadow">Connect 4</span><br> and CSS</h1>
 
 </div>
 
+
+<div id="board">
+
+
+</div>
+</section>
+
 <div id="popup1" class="overlay">
 	<div class="popup">
-		<h2 class="win"></h2>
+		<strong><h2 class="win"></h2></strong>
 		<a class="close" href="#">&times;</a>
 		<div class="content">
 			Great game! To play again, close this popup.
 		</div>
 	</div>
 </div>
-
-<div id="board">
-
-
-</div>
-
 
 <script src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
 <script src="javascripts/connect4.js"></script>
--- javascripts/connect4.js
+++ javascripts/connect4.js
@@ -1,19 +1,19 @@
-var backgrounds = [
-  "http://i.giphy.com/26BRLblDUw8VAhoFq.gif",
-  "http://i.giphy.com/qBhCZARUiS5WM.gif",
-  "http://i.giphy.com/11fSxqu3FUp2M0.gif",
-  "http://i.giphy.com/tzrlO0JiwjS7e.gif",
-  "http://i.giphy.com/3oriOeBHxdF9FokwJq.gif",
-  "http://i.giphy.com/mcT6IxQicH7hu.gif",
-  "http://i.giphy.com/l3q2ZlDOphy540eli.gif",
-  "http://i.giphy.com/26xBzu2ogAunL19hS.gif",
-  "http://i.giphy.com/NxWk4NY7C59Fm.gif",
-  "http://i.giphy.com/dGBiI0Wx9WPte.gif",
-  "http://i.giphy.com/BlcWQ9L2VfOFO.gif",
-  "http://i.giphy.com/3oEduH8ySCZMrUdytW.gif",
-  "http://i.giphy.com/UsqR2a5lOCzAI.gif",
-  "http://i.giphy.com/CLkMWh66GNkm4.gif"
-]
+// var backgrounds = [
+//   "http://i.giphy.com/26BRLblDUw8VAhoFq.gif",
+//   "http://i.giphy.com/qBhCZARUiS5WM.gif",
+//   "http://i.giphy.com/11fSxqu3FUp2M0.gif",
+//   "http://i.giphy.com/tzrlO0JiwjS7e.gif",
+//   "http://i.giphy.com/3oriOeBHxdF9FokwJq.gif",
+//   "http://i.giphy.com/mcT6IxQicH7hu.gif",
+//   "http://i.giphy.com/l3q2ZlDOphy540eli.gif",
+//   "http://i.giphy.com/26xBzu2ogAunL19hS.gif",
+//   "http://i.giphy.com/NxWk4NY7C59Fm.gif",
+//   "http://i.giphy.com/dGBiI0Wx9WPte.gif",
+//   "http://i.giphy.com/BlcWQ9L2VfOFO.gif",
+//   "http://i.giphy.com/3oEduH8ySCZMrUdytW.gif",
+//   "http://i.giphy.com/UsqR2a5lOCzAI.gif",
+//   "http://i.giphy.com/CLkMWh66GNkm4.gif"
+// ]
 
 class Connect4 {
   constructor() {
@@ -55,9 +55,9 @@ class Connect4 {
   endGame() {
 
 
-        $('.flexMe').fadeTo('400','lightyellow', function(event) {
-          $(this).css('background-color', 'lightyellow')
-        });
+        $('.flexMe').fadeTo('400','#e5ebf1', function(event) {
+          $(this).css('background-color', '#e5ebf1')
+        })
         $('.flexMe').attr('data-fill', 'false')
 
         this.array = [
@@ -71,17 +71,22 @@ class Connect4 {
 
         this.playCount = 0
         $('.win').html('');
-  
+
   }
 
 
   winActions(pos1) {
-    $('.win').text(`Player ${pos1} wins`)
+    $('.win').text(`Player ${pos1} Wins`)
+      $('.blur').css('filter', 'blur(5px)')
       $('.popup .content').text('Great game! To play again, close this popup.')
+      $('.overlay').css('visibility', 'visible')
     $('.popup').css('visibility', 'visible')
     $('.close').click((event) => {
+      $('.blur').css('filter', 'none')
       $('.popup .content').text('One moment, resetting the board...')
       $('.popup').css('visibility', 'hidden')
+      $('.overlay').css('visibility', 'hidden')
+
 
         this.endGame()
 
@@ -161,7 +166,7 @@ class Connect4 {
       if (children[i].getAttribute('data-fill') === 'false')
       currentChild = children[i]
     }
-    $('body').css('background-image', `url(${backgrounds[Math.floor(Math.random() * backgrounds.length)]})`)
+    // $('body').css('background-image', `url(${backgrounds[Math.floor(Math.random() * backgrounds.length)]})`)
     if (this.playCount % 2 === 0) {
       $(currentChild).css('background-color', 'rgba( 255 , 57 , 46 , 1 )')
       $(currentChild).attr('data-fill', 'true')
